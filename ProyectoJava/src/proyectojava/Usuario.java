c916769015f36113ef014c4fb25a411c65f8d46d
YATIENCIA <33614080+YATIENCIA@users.noreply.github.com>
2017-12-02 21:57:10 -0500

    Creación: Clase Usuario
    Se crea la clase Usuario y se escriben sus atributos, además existe un constructor vacío para futuro uso.

---
+++ ProyectoJava/src/proyectojava/Usuario.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectojava;
+
+/**
+ *
+ * @author IYAC
+ */
+public class Usuario {
+    
+    String nombre;
+    String apellido;
+    int edad;
+    String casa;// (Griffindor, Ravenclaw, Hufflepuff, Slytherin)
+    String varita;// (descripción de la varita)
+    String Tipo_de_mago;
+    String animal;
+    String pocion; 
+    String deporte;
+    String hechizo;
+
+
+    public Usuario() {
+        System.out.println("Instanciousuairo  vacio");
+    }
+
+}
+
+
